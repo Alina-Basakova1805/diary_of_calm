933755da0c4ee659b403e6ef65368aae4fdb902e
alina <basakovaal1805@gmail.com>
2025-06-11 15:17:34 +0700

    Добавила нужные файлы проекта

---
+++ diary_of_calm/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+# python3 manage.py runserver 7643
+
+urlpatterns = [
+    path('',views.home_page,name='home_page'),
+    path('entries/',views.my_entries,name ='my_entries')
+]
---
+++ diary_of_calm/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def home_page(request):
+    return render(request,('diary_of_calm/home_page.html'))
+
+def my_entries(request):
+    return render(request,'diary_of_calm/my_entries.html')
+
