603abf42579c16069912ff9ddecf8aa1db5d5461
alina <basakovaal1805@gmail.com>
2025-06-12 16:12:43 +0700

    Создала приложение users,внесла изменения в diary of calm

--- diary_of_calm/views.py
+++ diary_of_calm/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 def home_page(request):
     return render(request,('diary_of_calm/home_page.html'))
 
-def my_entries(request):
-    return render(request,'diary_of_calm/my_entries.html')
+# def my_entries(request):
+#     return render(request,'diary_of_calm/my_entries.html')
 
